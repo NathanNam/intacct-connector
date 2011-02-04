a9015a88d68ee78a11e003e86185aa16d82964a9
Martin Gontovnikas <gonto@zaubersoftware.com>
2011-02-04 16:20:53 -0300

    Added explanation of the String.intern() method usage

--- mule-module-intacct/src/main/java/org/mule/module/intacct/xml/XmlUnderscoreReplacementFilter.java
+++ mule-module-intacct/src/main/java/org/mule/module/intacct/xml/XmlUnderscoreReplacementFilter.java
@@ -13,7 +13,12 @@ package org.mule.module.intacct.xml;
 import org.xml.sax.Attributes;
 
 /**
- * This is the underscore filter
+ * This is the underscore filter Throuout this class methods we use the
+ * {@link String#intern()} method so as to get from a String the cached version. JaxB
+ * uses == instead of {@link Object#equals(Object)} to compare Strings, so they must
+ * be the same instance so as to work. As the String used to compare is the one in
+ * the annotation which already exists in the memory, we use intern to instruct the
+ * JVM to use that one so that the == comparation returrs true
  */
 public class XmlUnderscoreReplacementFilter implements XmlFilter
 {
@@ -72,8 +77,4 @@ public class XmlUnderscoreReplacementFilter implements XmlFilter
         return false;
     }
 
-    
-
 }
-
-
