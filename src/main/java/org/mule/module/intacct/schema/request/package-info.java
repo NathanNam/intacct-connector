8f31cf691f4e2cc08406e27b959c3ff9756c90b4
MuleSoft <code-contrib@mulesoft.com>
2011-10-06 08:22:15 -0700

    Updating documentation

--- src/main/java/org/mule/module/intacct/schema/request/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.mulesoft.org/schema/mule/intacct", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.mule.module.intacct.schema.request;
