f76206146b55d658000a22dfe3ad5e513db4c4cf
Federico Recio <federico.recio@mulesoft.com>
2011-11-15 12:12:57 -0300

    add parent pom & license header to all files

--- src/test/java/org/mule/module/intacct/impl/MockIntacctFacade.java
+++ src/test/java/org/mule/module/intacct/impl/MockIntacctFacade.java
@@ -1,6 +1,6 @@
 /*
- * $Id$
- * --------------------------------------------------------------------------------------
+ * Mule Intacct Cloud Connector
+ *
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
  * The software in this package is published under the terms of the CPAL v1.0
